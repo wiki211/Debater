89c56eef000476dd725322ce51c35b25df6b5a8c
CK <keepwiing@gmail.com>
2018-07-30 15:43:54 -0700

    Module modifications
    Added import sys to enable accessing of modules outside of typical directory.

--- addpy/__init__.py
+++ addpy/__init__.py
@@ -1,1 +1,3 @@
-__all__ = ["sessionselect","dataimport"]
+__all__ = ["sessionselect","dataimport","continuehandler","endhandler",
+"sessionselect","stancepresent","teamdisplay","teamselect",
+"timerpresent","topicpresent","votehandler","welcome"]
--- addpy/votehandler.py
+++ addpy/votehandler.py
@@ -1,2 +1,2 @@
-def test():
-    print("Hi!!!!!")
+import sys
+sys.path.insert(0, '/Users/demouser/Desktop/cssi-project/Debater/')
