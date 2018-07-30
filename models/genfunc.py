1537fe919df78752fff40f5caaf54383823874ab
CK <keepwiing@gmail.com>
2018-07-30 16:06:33 -0700

    Added genfunc
    Allows outside models to query using a specific function.

--- models/genfunc.py
+++ models/genfunc.py
@@ -6,3 +6,10 @@ def cleartype(modeltype):
 
 def cleartype(modeltype):
     ndb.delete_multi(modeltype.query().fetch(keys_only=True))
+
+def queryfield(modeltype, modprop, contentfilt="", typefilter=True):
+    if typefilter:
+        return modeltype.query().filter(modeltype.modprop==str(contentfilt)).fetch()
+    else:
+        return modeltype.query().order(modeltype.modprop).fetch()
+
