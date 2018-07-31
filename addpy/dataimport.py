a9db4b7c7fb026d6f4e6fd256c6b072aa99a727f
Michelle Pham <Michelle.pham707@gmail.com>
2018-07-31 15:47:30 -0700

    Sure

--- addpy/dataimport.py
+++ addpy/dataimport.py
@@ -1,9 +1,13 @@
 import csv
 import sys
 import os
-sys.path.insert(0, '/Users/demouser/Desktop/cssi-project/Debater/')
+sys.path.insert(0, '../')
 #these are necessary to change the syspath
+<<<<<<< HEAD
 from models import adminmodels, contentmodels, genfunc
+=======
+from models import contentmodels, genfunc
+>>>>>>> Michelle
 
 def getdata(pathtofile):
     fullpath = os.path.expanduser(str(pathtofile))
@@ -14,7 +18,7 @@ def getdata(pathtofile):
     return nn
 
 def importdata(impdata):
-    n = 0 #number of filled data entries 
+    n = 0 #number of filled data entries
     genfunc.cleartype(contentmodels.Topics)
     for r in impdata:
         dd = contentmodels.Topics(topic_content=r[0],topic_category=r[1],
@@ -26,4 +30,3 @@ def importdata(impdata):
 
 
 #data = getdata("/Users/demouser/Desktop/cssi-project/Debater/data/topics_cssi.csv")
-
