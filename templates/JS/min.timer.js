9bc1bd579eba3264d1a8a4aad12e0324bfd85c7b
Victoria Okoro <vicky05060@gmail.com>
2018-08-01 15:43:26 -0700

    Minute Timer
    HTML and CSS for minute timer

---
+++ templates/JS/min.timer.js
@@ -0,0 +1,26 @@
+var timeHandle;
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes
+    function click() {
+        var counter = document.getElementById("timer");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML =
+        current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            timeHandle=setTimeout(click, 1000);
+        } else {
+
+            if(mins > 1){
+
+               // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
+               setTimeout(function () { countdown(mins - 1); }, 1000);
+
+            }
+        }
+    }
+    click();
+}
+
+countdown(1);
