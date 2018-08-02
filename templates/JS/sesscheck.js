ee859ec68e1d06e9e74b69e764e992313c45b7f9
CK <keepwiing@gmail.com>
2018-08-01 22:16:54 -0700

    Infinite Animate
    Trying to add infinite animate

--- templates/JS/sesscheck.js
+++ templates/JS/sesscheck.js
@@ -9,10 +9,13 @@ function checksessid() {
                 window.location.replace("/round1?action=")
                 return true;
             } else {
-                document.getElementById('input_text').style ="animation: shake 0.5s; "
+                document.getElementById('input_text').style.animation = "none"
+                document.getElementById('input_text').style.cssText = null
+                console.log(document.getElementById('input_text').style.cssText)
+                document.getElementById('input_text').style ="animation: shake 0.5s;"
                 return false;
             }
-        };
+        }
     xmlHttp.open("GET", "/check/session?input_text="+encodeURIComponent(sessid), true); // true for asynchronous 
     xmlHttp.send(sessid);
 }
